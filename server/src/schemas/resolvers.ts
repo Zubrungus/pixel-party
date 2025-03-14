3befe850c15016b61e6751ed3cf21ec0e41b8006
ant-codes-42 <schwab.it@pm.me>
2025-03-14 15:02:11 -0500

    initial commit - scaffolding of project complete

---
+++ server/src/schemas/resolvers.ts
@@ -0,0 +1,15 @@
+// Import Model here
+import { signToken, AuthenticationError } from '../utils/auth.js';
+
+// Interfaces go here
+
+const resolvers = {
+  Query: {
+    // Add queries here
+  },
+  Mutation: {
+    // Add mutations here
+  },
+};
+
+export default resolvers;
---
+++ server/src/schemas/typeDefs.ts
@@ -0,0 +1,13 @@
+const typeDefs = `
+  types go here
+
+  type Query {
+    queries go here
+  }
+
+  type Mutation {
+    mutations go here
+  }
+`;
+
+export default typeDefs;
