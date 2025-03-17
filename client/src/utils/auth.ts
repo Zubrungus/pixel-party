b2222279d50574533855122150079544a65a200c
rasersharpe <jay.bhatt@me.com>
2025-03-17 11:06:11 -0700

    Created client side mutations.ts and queries.ts.

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,11 +1,31 @@
-import jwtDecode from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 
 const TOKEN_KEY = "id_token";
 
-export const getToken = () => {};
+export const getToken = (): string | null => localStorage.getItem(TOKEN_KEY);
 
-export const saveToken = (token: string) => {};
+export const removeToken = (): void => {
+  localStorage.removeItem(TOKEN_KEY);
+};
 
-export const removeToken = () => {};
+export const isAuthenticated = (): boolean => {
+  const token = getToken();
 
-export const isAuthenticated = () => {};
+  if (!token) {
+    removeToken(); // No token, remove it
+    return false;
+  }
+
+  try {
+    const decoded = jwtDecode<{ exp: number }>(token);
+    if (decoded.exp * 1000 < Date.now()) {
+      removeToken(); // Expired, remove it
+      return false;
+    }
+    return true;
+  } catch (err) {
+    removeToken(); // Invalid token, remove it
+    console.error(err);
+    return false;
+  }
+};
