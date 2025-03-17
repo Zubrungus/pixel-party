89813739d5c8794c1fffa91fb3f620a4c0b9df67
rasersharpe <jay.bhatt@me.com>
2025-03-17 09:37:27 -0700

    Created client side auth.ts and outlined necessary function expressions.

---
+++ client/src/utils/auth.ts
@@ -0,0 +1,11 @@
+import jwtDecode from "jwt-decode";
+
+const TOKEN_KEY = "id_token";
+
+export const getToken = () => {};
+
+export const saveToken = (token: string) => {};
+
+export const removeToken = () => {};
+
+export const isAuthenticated = () => {};
