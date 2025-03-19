c124770c14522567499f0739342f011928ac1be9
rasersharpe <jay.bhatt@me.com>
2025-03-19 12:07:36 -0700

    Fixed 2 case sensitive imports in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,14 @@
-import { useState, useEffect } from 'react'
-import { useQuery, useMutation, useSubscription, gql } from '@apollo/client'
-import './index.css'
-import { Canvas } from './canvas/canvas'
-import { CanvasOverlay } from './canvas/canvasOverlay';
-import { ColorSelector } from './components/ColorSelector';
-import { ConfirmButton } from './components/ConfirmButton';
-import { ToggleGrid } from './components/ToggleGrid';
-import Header from './components/Header';
-import { ZoomButtons } from './components/ZoomButtons';
-import { getUser } from './utils/auth';
+import { useState, useEffect } from "react";
+import { useQuery, useMutation, useSubscription, gql } from "@apollo/client";
+import "./index.css";
+import { Canvas } from "./Canvas/canvas";
+import { CanvasOverlay } from "./Canvas/canvasOverlay";
+import { ColorSelector } from "./components/ColorSelector";
+import { ConfirmButton } from "./components/ConfirmButton";
+import { ToggleGrid } from "./components/ToggleGrid";
+import Header from "./components/Header";
+import { ZoomButtons } from "./components/ZoomButtons";
+import { getUser } from "./utils/auth";
 // GraphQL queries and mutations
 const GET_ALL_PIXELS = gql`
   query GetAllPixels {
@@ -72,7 +72,9 @@ function App() {
   const [offsetX, setOffestX] = useState(0);
   const [offsetY, setOffestY] = useState(0);
   // Current state of imageData, which is the 100 by 100 grid of pixels
-  const [imageData, setImageData] = useState(new Uint8ClampedArray(40000).fill(255)); // Initialize with transparent pixels
+  const [imageData, setImageData] = useState(
+    new Uint8ClampedArray(40000).fill(255)
+  ); // Initialize with transparent pixels
 
   // Query to get all pixels
   const { loading, error, data } = useQuery(GET_ALL_PIXELS);
@@ -86,7 +88,7 @@ function App() {
       if (data?.data?.pixelUpdated) {
         updateCanvasWithPixel(data.data.pixelUpdated);
       }
-    }
+    },
   });
 
   // Update canvas with initial pixels when data is loaded
@@ -96,11 +98,11 @@ function App() {
 
       // Set default canvas to white
       data.getAllPixels.forEach((_pixel: Pixel, i: number) => {
-        newImageData[i] = 255;     // R
+        newImageData[i] = 255; // R
         newImageData[i + 1] = 255; // G
         newImageData[i + 2] = 255; // B
         newImageData[i + 3] = 255; // A
-      })
+      });
 
       // Apply pixels from database
       data.getAllPixels.forEach((pixel: Pixel) => {
@@ -115,61 +117,94 @@ function App() {
   const applyPixelToImageData = (imgData: Uint8ClampedArray, pixel: Pixel) => {
     const index = (pixel.y * 100 + pixel.x) * 4;
 
-    let r: number = 255, g: number = 255, b: number = 255;
+    let r: number = 255,
+      g: number = 255,
+      b: number = 255;
 
     switch (pixel.color) {
       case "1":
-        r = 179; g = 41; b = 41;
+        r = 179;
+        g = 41;
+        b = 41;
         break;
       case "2":
-        r = 67; g = 56; b = 214;
+        r = 67;
+        g = 56;
+        b = 214;
         break;
       case "3":
-        r = 223; g = 119; b = 22;
+        r = 223;
+        g = 119;
+        b = 22;
         break;
       case "4":
-        r = 56; g = 129; b = 224;
+        r = 56;
+        g = 129;
+        b = 224;
         break;
       case "5":
-        r = 232; g = 204; b = 23;
+        r = 232;
+        g = 204;
+        b = 23;
         break;
       case "6":
-        r = 117; g = 225; b = 225;
+        r = 117;
+        g = 225;
+        b = 225;
         break;
       case "7":
-        r = 37; g = 131; b = 40;
+        r = 37;
+        g = 131;
+        b = 40;
         break;
       case "8":
-        r = 101; g = 199; b = 56;
+        r = 101;
+        g = 199;
+        b = 56;
         break;
       case "9":
-        r = 213; g = 77; b = 177;
+        r = 213;
+        g = 77;
+        b = 177;
         break;
       case "10":
-        r = 211; g = 150; b = 175;
+        r = 211;
+        g = 150;
+        b = 175;
         break;
       case "11":
-        r = 120; g = 86; b = 59;
+        r = 120;
+        g = 86;
+        b = 59;
         break;
       case "12":
-        r = 179; g = 136; b = 101;
+        r = 179;
+        g = 136;
+        b = 101;
         break;
       case "13":
-        r = 89; g = 89; b = 89;
+        r = 89;
+        g = 89;
+        b = 89;
         break;
       case "14":
-        r = 147; g = 147; b = 154;
+        r = 147;
+        g = 147;
+        b = 154;
         break;
       case "15":
-        r = 0; g = 0; b = 0;
+        r = 0;
+        g = 0;
+        b = 0;
         break;
       case "16":
-        r = 255; g = 255; b = 255;
+        r = 255;
+        g = 255;
+        b = 255;
         break;
-
     }
 
-    imgData[index] = r;     // R
+    imgData[index] = r; // R
     imgData[index + 1] = g; // G
     imgData[index + 2] = b; // B
     imgData[index + 3] = 255; // A (fully opaque)
@@ -184,12 +219,11 @@ function App() {
 
   // Divide by 5 and round down to get specific pixel clicked
   function handleClickX(pos: number) {
-    setClickX(Math.floor((pos / 5) / scaleLevel));
+    setClickX(Math.floor(pos / 5 / scaleLevel));
   }
 
   function handleClickY(pos: number) {
-    setClickY(Math.floor((pos / 5) / scaleLevel));
-
+    setClickY(Math.floor(pos / 5 / scaleLevel));
   }
 
   function handleClickedColor(color: number) {
@@ -221,15 +255,15 @@ function App() {
     //Make sure that X and Y are not their default values
     if (clickX >= 0 && clickY >= 0 && user) {
       createPixel({
-        variables: { x: clickX, y: clickY, color: clickedColor.toString() }
+        variables: { x: clickX, y: clickY, color: clickedColor.toString() },
       })
-        .then(response => {
+        .then((response) => {
           console.log("Pixel created successfully:", response);
           // Reset click position after successful pixel placement
           setClickX(-1);
           setClickY(-1);
         })
-        .catch(err => {
+        .catch((err) => {
           console.error("Error placing pixel:", err);
           // Display error to user if authentication fails
           if (err.message.includes("logged in")) {
@@ -247,7 +281,7 @@ function App() {
   }
 
   function handleZoomIncrease() {
-    setScaleLevel(scaleLevel + 1)
+    setScaleLevel(scaleLevel + 1);
   }
 
   function handleZoomDecrease() {
@@ -265,24 +299,52 @@ function App() {
   return (
     <>
       <Header />
-      
-      <div style={{ marginTop: '80px' }}> {/* Add margin to account for fixed header */}
-        <div id="draggableWrapper" style={{ transform: `translate(${offsetX}px, ${offsetY}px)` }} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} onMouseLeave={handleMouseUp} onMouseMove={handleMouseMove} >
-          <div id="canvasWrapper" style={{ transform: ` scale(${scaleLevel})`, imageRendering: `pixelated` }} >
-            <div className="canvas pixelCanvasMagnifier" >
+
+      <div style={{ marginTop: "80px" }}>
+        {" "}
+        {/* Add margin to account for fixed header */}
+        <div
+          id="draggableWrapper"
+          style={{ transform: `translate(${offsetX}px, ${offsetY}px)` }}
+          onMouseDown={handleMouseDown}
+          onMouseUp={handleMouseUp}
+          onMouseLeave={handleMouseUp}
+          onMouseMove={handleMouseMove}
+        >
+          <div
+            id="canvasWrapper"
+            style={{
+              transform: ` scale(${scaleLevel})`,
+              imageRendering: `pixelated`,
+            }}
+          >
+            <div className="canvas pixelCanvasMagnifier">
               <Canvas height={100} width={100} imageData={imageData} />
             </div>
-            <CanvasOverlay height={500} width={500} updateClickX={handleClickX} updateClickY={handleClickY} lastClickX={clickX} lastClickY={clickY} gridToggle={gridToggle} />
+            <CanvasOverlay
+              height={500}
+              width={500}
+              updateClickX={handleClickX}
+              updateClickY={handleClickY}
+              lastClickX={clickX}
+              lastClickY={clickY}
+              gridToggle={gridToggle}
+            />
           </div>
         </div>
-
-        <ColorSelector clickedColorHandler={handleClickedColor} clickedColor={clickedColor} />
+        <ColorSelector
+          clickedColorHandler={handleClickedColor}
+          clickedColor={clickedColor}
+        />
         <ConfirmButton confirmHandler={handleConfirm} />
-        <ZoomButtons increaseZoomHandler={handleZoomIncrease} decreaseZoomHandler={handleZoomDecrease} />
+        <ZoomButtons
+          increaseZoomHandler={handleZoomIncrease}
+          decreaseZoomHandler={handleZoomDecrease}
+        />
         <ToggleGrid toggleGridHandler={handleToggleGrid} />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
